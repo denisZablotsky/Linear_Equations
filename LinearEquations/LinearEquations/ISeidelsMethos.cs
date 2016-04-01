a1355d3a2b34df28c667e89901a0dd2f95349d9c
Denis Zablotsky <dZablotsky@yandex.ru>
2016-04-01 12:38:29 +0600

    Interface for Seidels Method class was added.

---
+++ LinearEquations/LinearEquations/ISeidelsMethos.cs
@@ -0,0 +1,13 @@
+﻿
+namespace LinearEquations
+{
+    public interface ISeidelsMethos
+    {
+        public static int[] calculate(int[,] array, int size);
+        // Cтатический метод calculate принимает на вход
+        // матрицу коэффициентов размера size*size+1, 
+        // само значение размера матрицы size.
+        // На выход должна быть возвращена одномерная 
+        // матрица решений размера size
+    }
+}
