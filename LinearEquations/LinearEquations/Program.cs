9debfc7bba30b93acb74e987951f374fa0dcc13d
Denis Zablotsky <dZablotsky@yandex.ru>
2016-04-03 18:44:01 +0600

    Choice of method input for user was added.

--- LinearEquations/LinearEquations/Program.cs
+++ LinearEquations/LinearEquations/Program.cs
@@ -7,7 +7,12 @@ namespace LinearEquations
         static void Main(string[] args)
         {
             int size = 0;
-            IConsoleInput consoleInput = new FileInput();
+            IConsoleInput consoleInput;
+            Console.WriteLine("Enter method of input data: Press f to input via FILE or another key to manual input:");
+            if (Console.ReadKey(true).Key == ConsoleKey.F)
+                consoleInput = new FileInput();
+            else
+                consoleInput = new ConsoleInput();
             ISeidelsMethod seidel = new SeidelsMethod();
             double[,] array = consoleInput.ConsoleReadDate(out size);
             double[] res = seidel.calculate(array, size);
